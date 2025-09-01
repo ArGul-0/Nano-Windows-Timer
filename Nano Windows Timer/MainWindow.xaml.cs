b6afdd2d2cfae98d418b9eff5defb7bcad489ca9
ArGul-0 <argul.mail@gmail.com>
2025-09-02 02:32:45 +0300

    Started worked on base functions.

--- Nano Windows Timer/MainWindow.xaml.cs
+++ Nano Windows Timer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Linq;
+using System.Windows.Threading;
 
 namespace Nano_Windows_Timer
 {
@@ -9,24 +10,57 @@ namespace Nano_Windows_Timer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static bool isRunning = false;
+
+        private DispatcherTimer timer;
+        private int seconds = 0;
         public MainWindow()
         {
             InitializeComponent();
+
+            TimerTextBlock.Text = TimeSpan.FromSeconds(seconds).ToString(@"hh\:mm\:ss");
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
+            if(isRunning)
+            {
+                return;
+            }
 
+            isRunning = true;
+            timer.Start();
         }
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if(!isRunning)
+            {
+                return;
+            }
+            isRunning = false;
+            timer.Stop();
         }
 
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
+            if(isRunning)
+            {
+                isRunning = false;
+                timer.Stop();
+            }
+        }
 
+
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            seconds++;
+            TimerTextBlock.Text = TimeSpan.FromSeconds(seconds).ToString(@"hh\:mm\:ss");
         }
     }
 }
